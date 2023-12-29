5bffe231982fb726048c2053837921423254efb2
nfor2000 <nforpresly2@gmail.com>
2023-12-29 15:45:54 +0100

    api removed

--- src/App.jsx
+++ src/App.jsx
@@ -41,20 +41,6 @@ function App() {
 
           window.addEventListener('scroll', scrollHeader);
 
-          /* Hero type effect */
-          const typed = document.querySelector('.typed');
-
-          if(typed) {
-               let typed_strings = typed.getAttribute('data-typed-items');
-               typed_strings = typed_strings.split(',');
-               new Typed('.typed', {
-                    strings:typed_strings,
-                    loop:true,
-                    typeSpeed: 100,
-                    backSpeed:50,
-                    backDelay:2000
-               })
-          }
          
           // Scroll sections active Link
 
