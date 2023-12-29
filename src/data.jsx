6cfd0b646cd9051c63383fb71062ebfa346d0202
nfor2000 <nforpresly2@gmail.com>
2023-12-29 14:26:14 +0100

    api removed

--- src/data.jsx
+++ src/data.jsx
@@ -1,22 +1,22 @@
 const data = [
      {
           "title": "Quiz App",
-          "imgUrl": "./src/assets/project-1.png",
+          "imgUrl": "./public/project-1.png",
           "link": "https://nfor2000.github.io/Quiz-Web-app/"
      },
      {
           "title": "Age Calculator",
-          "imgUrl": "./src/assets/project-2.png",
+          "imgUrl": "./public/project-2.png",
           "link": "https://nfor2000.github.io/age-calculator-app-main/"
      },
      {
           "title": "Expenses chart",
-          "imgUrl": "./src/assets/project-3.png",
+          "imgUrl": "./public/project-3.png",
           "link": "https://nfor2000.github.io/expenses-chart-component-main/"
      },
      {
           "title": "Kleanstar website",
-          "imgUrl": "./src/assets/project-4.png",
+          "imgUrl": "./public/project-4.png",
           "link": "https://nfor2000.github.io/klean-construction-site/"
      }
 ];
